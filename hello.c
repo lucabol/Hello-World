931cc63c9c1b156734f873fcb153fac271b91fed
Luca Bolognese <lucabol@microsoft.com>
2025-09-05 10:03:32 +0200

    Reset hello.c for testing

--- hello.c
+++ hello.c
@@ -1,9 +1,5 @@
 # include <stdio.h>
  
 int main(){
-    printf("Ciao, Mondo!\n\n");
- 
-    int exit_code = 0;
-    printf("Exit code: %d\n", exit_code);
-    return exit_code;
+    printf("Hello world!\n\n");
 }
