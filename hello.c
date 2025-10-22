c5261e78db72c6bae01ca3c07cfa3f4084da7e6c
Luca Bolognese <lucabol@microsoft.com>
2025-10-22 13:21:42 +0200

    Reset baseline hello.c for repo reset

--- hello.c
+++ hello.c
@@ -1,19 +1,5 @@
-#include <stdio.h>
-#include "hello.h"
+# include <stdio.h>
 
-/* get_greeting - Returns the greeting message
- * This function provides the core greeting functionality
- */
-const char* get_greeting(void) {
-    return "Hello world!";
+int main(){
+    printf("Hello world!");
 }
-
-#ifndef UNIT_TEST
-/* main - Entry point of the program
- * Prints the greeting message followed by a newline
- */
-int main(void) {
-    printf("%s\n", get_greeting());
-    return 0;
-}
-#endif
