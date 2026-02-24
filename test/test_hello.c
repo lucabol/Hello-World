3ea460c69086472037e0e7065a8be1fcadf0e25e
Luca Bolognese <lucabol@microsoft.com>
2026-02-24 08:41:14 +0100

    Remove test/test_hello.c for repo reset

--- test/test_hello.c
+++
@@ -1,8 +0,0 @@
-// Unit tests for hello.c
-// Stub test - actual unit testing would require refactoring the main function
-#include <stdio.h>
-
-int main() {
-    printf("Unit tests: All tests passed\n");
-    return 0;
-}
