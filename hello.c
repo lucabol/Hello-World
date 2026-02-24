e3e7b18c3eb9a1872f3595f4eb36df3ac904e534
Luca Bolognese <lucabol@microsoft.com>
2026-02-24 09:22:32 +0100

    Reset baseline hello.c for repo reset

--- hello.c
+++ hello.c
@@ -1,12 +1,5 @@
-#include <stdio.h>
+# include <stdio.h>
 
-void print_message(const char *msg) {
-    puts(msg);
-}
-
-int main(void) {
-    int exit_code = 0;
-    print_message("Hello world!");
-    printf("Exit code: %d\n", exit_code);
-    return exit_code;
+int main(){
+    printf("Hello world!");
 }
